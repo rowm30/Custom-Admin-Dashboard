54a3effb35fa4eef0a5847f60e2d374bbcc7d802
rowm30 <rowm30@gmail.com>
2021-05-15 21:13:06 +0530

    In_my_Hands

--- src/app/shared/member.model.ts
+++ src/app/shared/member.model.ts
@@ -1,2 +1,7 @@
 export class Member {
+    _id: string="";
+    name: string="";
+    batch: string="";
+    role: string="";
+    votes: number=0;
 }
--- src/app/shared/member.service.ts
+++ src/app/shared/member.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
+
+import { Member } from './member.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberService {
+  selectedMember: Member = new Member;
+  members: Member[] = [];
 
   constructor() { }
 }
